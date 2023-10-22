61e08c3fbca3defa6f34318b351757eb8b743058
Harkeerat Singh Sawhney <harkeerat@gmail.com>
2023-10-22 17:03:31 +0200

    Exercise 20 Completed

--- Projects/Project 1/harkeerat_sawhney.py
+++ Projects/Project 1/harkeerat_sawhney.py
@@ -5,19 +5,15 @@ Student: Harkeerat Singh Sawhney
 # Libraries:
 import matplotlib.pyplot as plt
 import numpy as np
-import platform
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import time
 import os
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-from sklearn.linear_model import LinearRegression
 
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+# os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 params = {
     "text.usetex": True,
@@ -55,13 +51,22 @@ color = "b"
 
 
 # *** Question 2 **
-def create_dataset(coeffs, z_range, sample_size, sigma, seed=42):
+def create_dataset(
+    coeffs=[1, 2, 3, 4, 5],
+    z_range=np.array([0, -10, 1, -1, 1 / 1000]),
+    sample_size=10,
+    sigma=0.1,
+    seed=42,
+):
     print("Starting Exercise 2")
-    random_state = np.random.RandomState(seed)
-    x_min, x_max = z_range
-    X = random_state.uniform(x_min, x_max, sample_size)
 
-    epsilon = random_state.normal(0, sigma, sample_size)
+    random_state = np.random.RandomState(seed)
+
+    x_min, x_max = z_range
+
+    X = random_state.uniform(x_min, x_max, (sample_size))
+
+    epsilon = random_state.normal(0.0, sigma, sample_size)
 
     y = (
         coeffs[0]
@@ -92,31 +97,17 @@ def visualize_data(X, y, coeffs, z_range, title=""):
     plt.show()
 
 
-# class MyDataset(Dataset):
-#     def __init__(self, X, y):
-#         self.X = X
-#         self.y = y
+class PolynomialRegressionModel(nn.Module):
+    def __init__(self, input_dim, output_dim, device):
+        super(PolynomialRegressionModel, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 100, bias=True).to(device)
+        self.fc2 = nn.Linear(100, output_dim, bias=True).to(device)
+        self.device = device
 
-#     def __getitem__(self, index):
-#         return (self.X[index], self.y[index])
-
-#     def __len__(self):
-#         return len(self.X)
-
-# class MyModel(nn.Module):
-#     def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
-#         super(MyModel, self).__init__()
-#         self.fc1 = nn.Linear(input_size, hidden_size1)
-#         self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-#         self.fc3 = nn.Linear(hidden_size2, output_size)
-
-#     def forward(self, x):
-#         x = self.fc1(x)
-#         x = F.relu(x)
-#         x = self.fc2(x)
-#         x = F.relu(x)
-#         x = self.fc3(x)
-#         return x
+    def forward(self, x):
+        x = torch.relu(self.fc1(x)).to(self.device)
+        x = self.fc2(x).to(self.device)
+        return x
 
 
 if __name__ == "__main__":
@@ -133,10 +124,11 @@ if __name__ == "__main__":
     X_train, y_train = create_dataset(
         coeffs, z_range, sample_size_train, sigma, seed_train
     )
+
     X_eval, y_eval = create_dataset(coeffs, z_range, sample_size_eval, sigma, seed_eval)
 
-    visualize_data(X_train, y_train, coeffs, z_range, title="Training Data")
-    visualize_data(X_eval, y_eval, coeffs, z_range, title="Evaluation Data")
+    # visualize_data(X_train, y_train, coeffs, z_range, title="Training Data")
+    # visualize_data(X_eval, y_eval, coeffs, z_range, title="Evaluation Data")
 
     # *** Question 5 **
     print("Starting Exercise 5")
@@ -148,55 +140,79 @@ if __name__ == "__main__":
     else:
         DEVICE = torch.device("cpu")
 
+    DEVICE = torch.device("cuda:0")
+
     # Print the selected device
     print("Using device:", DEVICE)
 
-    # Check if GPU is available
-    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Reshaping the Data to Polynomial Features
+    X_train = X_train.reshape(-1, 1)
+    X_eval = X_eval.reshape(-1, 1)
 
-    # Convert the data to tensors and move to GPU
-    X_train = torch.tensor(X_train, dtype=torch.float32).unsqueeze(1).to(DEVICE)
-    y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1).to(DEVICE)
-    X_eval = torch.tensor(X_eval, dtype=torch.float32).unsqueeze(1).to(DEVICE)
-    y_eval = torch.tensor(y_eval, dtype=torch.float32).unsqueeze(1).to(DEVICE)
+    degree = 4
 
-    degree = 2
-    model = nn.Sequential(nn.Linear(degree, 1)).to(DEVICE)
+    # X_train = model.fit_transform(X_train)
+    # X_eval = model.fit_transform(X_eval)
+    input_dim = X_train.shape[1]
+    output_dim = 1
 
-    loss_function = nn.MSELoss()
+    model = PolynomialRegressionModel(input_dim, output_dim, DEVICE)
 
-    learning_rate = 0.1
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    criteria = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+    # Shape expected by nn.Linear
+    X_train = X_train.reshape(sample_size_train, 1)
+    y_train = y_train.reshape(sample_size_train, 1)
+    X_eval = X_eval.reshape(sample_size_eval, 1)
+    y_eval = y_eval.reshape(sample_size_eval, 1)
+
+    # Convert everything to torch
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32)
+    X_eval = torch.tensor(X_eval, dtype=torch.float32)
+    y_eval = torch.tensor(y_eval, dtype=torch.float32)
+
+    # Move everything to the device you want to use
+    X_train = X_train.to(DEVICE)
+    y_train = y_train.to(DEVICE)
+    X_eval = X_eval.to(DEVICE)
+    y_eval = y_eval.to(DEVICE)
+
+    initial_model_value = model(X_train)
+
+    print(
+        criteria(initial_model_value, torch.tensor(y_train).reshape(-1, 1).to(DEVICE))
+    )
 
     # Train the model
     num_epochs = 3000
 
     for epoch in range(num_epochs):
-        # Training
         model.train()
+        outputs = model(X_train)
+        loss = criteria(outputs, y_train)
+
+        # Backward pass and optimization
         optimizer.zero_grad()
-        y_hat = model(X_train.pow(torch.arange(degree, dtype=torch.float32)).to(DEVICE))
-        loss = loss_function(y_hat, y_train)
         loss.backward()
         optimizer.step()
 
-        # Evaluation
-        model.eval()
         with torch.no_grad():
-            y_hat_eval = model(X_eval.pow(torch.arange(degree, dtype=torch.float32)).to(DEVICE))
-            loss_eval = loss_function(y_hat_eval, y_eval)
-            if epoch % 10 == 0:
+            outputs_eval = model(X_eval)
+            loss_eval = criteria(outputs_eval, y_eval)
+
+            if (epoch + 1) % 100 == 0:
                 print(
-                    "Epoch:",
-                    epoch,
-                    "Loss:",
-                    loss.item(),
-                    "Loss eval:",
-                    loss_eval.item(),
+                    "Epoch [{}/{}], Loss: {:.4f}".format(
+                        epoch + 1, num_epochs, loss.item()
+                    )
                 )
 
-    # Print the final weights and bias
-    print("Final weights:", model[0].weight, "Final bias:\n", model[0].bias)    
+                if loss.item() < 0.5:
+                    break
+
+    print("Final loss:", loss.item())
 
     # *** Question 6 **
 
